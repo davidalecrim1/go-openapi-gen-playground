6464f066f9fb2dcb7d4323390b493a630143b30b
David Alecrim <dsalecrim@outlook.com>
2024-12-09 17:10:24 -0300

    feat: add sample data in the handler

--- internal/api/handler/handler.go
+++ internal/api/handler/handler.go
@@ -12,32 +12,56 @@ func NewTodoAPI() *TodoAPI {
 	return &TodoAPI{}
 }
 
+var ptr = func(s string) *string { return &s }
+
 // Get all To-Do items
 // (GET /todos)
 func (t TodoAPI) GetTodos(w http.ResponseWriter, r *http.Request) *generated.Response {
-	panic("not implemented") // TODO: Implement
+	return generated.GetTodosJSON500Response(
+		generated.ErrorResponse{
+			Error: "Internal Server Error",
+		},
+	)
 }
 
 // Create a new To-Do item
 // (POST /todos)
 func (t TodoAPI) PostTodos(w http.ResponseWriter, r *http.Request) *generated.Response {
-	panic("not implemented") // TODO: Implement
+	return generated.PostTodosJSON201Response(
+		generated.Todo{
+			ID:          "1234",
+			Title:       "foo",
+			Description: ptr("Bar"),
+			Completed:   false,
+		},
+	)
 }
 
 // Delete a specific To-Do item
 // (DELETE /todos/{id})
 func (t TodoAPI) DeleteTodosID(w http.ResponseWriter, r *http.Request, id string) *generated.Response {
-	panic("not implemented") // TODO: Implement
+	return &generated.Response{
+		Code: 204,
+	}
 }
 
 // Get a specific To-Do item
 // (GET /todos/{id})
 func (t TodoAPI) GetTodosID(w http.ResponseWriter, r *http.Request, id string) *generated.Response {
-	panic("not implemented") // TODO: Implement
+	todo := generated.Todo{
+		ID:          id,
+		Title:       "foo",
+		Description: ptr("bar"),
+		Completed:   false,
+	}
+
+	return generated.GetTodosIDJSON200Response(todo)
 }
 
 // Update a specific To-Do item
 // (PUT /todos/{id})
 func (t TodoAPI) PutTodosID(w http.ResponseWriter, r *http.Request, id string) *generated.Response {
-	panic("not implemented") // TODO: Implement
+	return &generated.Response{
+		Code: 404,
+	}
 }
